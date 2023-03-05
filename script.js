b36acaef1a2a372e7775720766763aa9c33f7777
Luke Spicknall <lukejspicknall@gmail.com>
2023-03-05 18:56:22 -0500

    add Book object from practice lesson to .js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 let book = "hello";
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    console.log(`${title} by ${author}, ${pages} pages, ${read}.`);
+  };
+}
+const bookOne = new Book("The Hobbit", "J.R.R Tolkein", "295", "have not read");
+
+// console.log(bookOne.info());
