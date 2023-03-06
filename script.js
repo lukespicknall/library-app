3f561ba2f2ffe48590f228aa93dd104905f2db5a
Luke Spicknall <lukejspicknall@gmail.com>
2023-03-05 20:08:42 -0500

    start js logic with myLibrary[] and functions to send book objects into that array. add some display divs to .html

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const myLibary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    console.log(`${title} by ${author}, ${pages} pages, ${read}.`);
-  };
+  // this.info = function () {
+  //   console.log(`${title} by ${author}, ${pages} pages, ${read}.`);
+  // };
 }
-const bookOne = new Book("The Hobbit", "J.R.R Tolkein", "295", "have not read");
+const bookOne = new Book('The Hobbit', 'J.R.R Tolkein', '295', 'have not read');
+const bookTwo = new Book('Riven Rock', 'T.C. Boyle', '362', 'have read');
+
+function addBook(a, b) {
+  myLibary.push(a, b);
+}
+
+addBook(bookOne, bookTwo);
 
 // console.log(bookOne.info());
