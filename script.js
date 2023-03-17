ab22a799794e5d4c91c74fcc518027a40921f657
Luke Spicknall <lukejspicknall@gmail.com>
2023-03-17 17:19:35 -0400

    form data now pushes into myLibrary array. Need to get array to display in table

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 const libTable = document.querySelector('.lib-table');
-const tBody = document.querySelector('tbody');
+// const tBody = document.querySelector('tbody');
+const addBtn = document.querySelector('.add-btn');
+const sideForm = document.querySelector('.side-form');
 
-const myLibary = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  // this.info = function () {
-  //   console.log(`${title} by ${author}, ${pages} pages, ${read}.`);
-  // };
-}
-const bookOne = new Book('The Hobbit', 'J.R.R Tolkein', '295', 'have not read');
-// const bookTwo = new Book('Riven Rock', 'T.C. Boyle', '362', 'have read');
-
-function addBook(a) {
-  myLibary.push(a);
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-addBook(bookOne);
+const myLibary = Object.values(Book);
+
+function addBook() {
+  const title = document.querySelector('#book-title').value;
+  const author = document.querySelector('#author-name').value;
+  const pages = document.querySelector('#page-count').value;
+  const read = document.querySelector('#have-read').value;
+  const newBook = new Book(title, author, pages, read);
+  myLibary.push(newBook);
+  for (let i = 0; i < myLibary.length; i += 1) {
+    ;
+  }
+}
+
+addBtn.addEventListener('click', (e) => {
+  sideForm.style.display = 'block';
+});
+
+sideForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBook();
+  sideForm.style.display = 'none';
+});
+
+// addBook(bookOne);
 
 // myLibary.forEach(a => {
 //   libTable.textContent = myLibary[a];
 // });
 
-for (let i = 0; i < myLibary.length; i += 1) {
-  
-}
+// }
 
-
-
-
-
-
-
-
-
-libTable.appendChild = myLibary;
+// libTable.appendChild = myLibary;
 
 // console.log(bookOne.info());
