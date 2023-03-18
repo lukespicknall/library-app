880d7997db542fe21c7a97fef2ee25c66fa723f9
Luke Spicknall <lukejspicknall@gmail.com>
2023-03-18 16:39:57 -0400

    creat displayBook() function to create rows to hold books objects and append them to the library table, create cells to hold book object values and append them to the new rows.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const libTable = document.querySelector('.lib-table');
-// const tBody = document.querySelector('tbody');
+// const libTable = document.querySelector('libTable');
+const tBody = document.querySelector('tbody');
 const addBtn = document.querySelector('.add-btn');
 const sideForm = document.querySelector('.side-form');
 
@@ -12,38 +12,45 @@ class Book {
   }
 }
 
-const myLibary = Object.values(Book);
+const myLibary = Object.values(Book); // creates array of Book objects/values
+
+function displayBook() {
+  tBody.innerHTML = ''; // clears current tbody to avoid repeats
+  for (let i = 0; i < myLibary.length; i += 1) { // loop throu []
+    const row = document.createElement('tr'); // create new tr for new Book
+    tBody.appendChild(row); // add that tr to tbody in libTable
+    const titleCell = document.createElement('td');
+    const authorCell = document.createElement('td');
+    const pagesCell = document.createElement('td'); // create new cells for Book data
+    const readCell = document.createElement('td');
+    titleCell.textContent = myLibary[i].title;
+    authorCell.textContent = myLibary[i].author;
+    pagesCell.textContent = myLibary[i].pages; // assing object value to cell content
+    readCell.textContent = myLibary[i].read;
+    row.appendChild(titleCell);
+    row.appendChild(authorCell);
+    row.appendChild(pagesCell); // add those cells to the new row
+    row.appendChild(readCell);
+  }
+}
 
 function addBook() {
   const title = document.querySelector('#book-title').value;
   const author = document.querySelector('#author-name').value;
-  const pages = document.querySelector('#page-count').value;
-  const read = document.querySelector('#have-read').value;
-  const newBook = new Book(title, author, pages, read);
-  myLibary.push(newBook);
-  for (let i = 0; i < myLibary.length; i += 1) {
-    ;
-  }
+  const pages = document.querySelector('#page-count').value; // find value of each form input
+  const read = document.querySelector('input[name="have-read"]').value;
+  const newBook = new Book(title, author, pages, read); // creates new Book object with those values
+  myLibary.push(newBook); // pushes that new book object into myLib[]
+  displayBook();
 }
 
 addBtn.addEventListener('click', (e) => {
-  sideForm.style.display = 'block';
+  sideForm.reset(); // clears form inputs from previous submission
+  sideForm.style.display = 'block'; // makes form appear
 });
 
 sideForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addBook();
-  sideForm.style.display = 'none';
+  e.preventDefault(); // stops sumbit from sending data to server by default
+  addBook(); // sends data to myLib[]
+  sideForm.style.display = 'none'; // makes form dissapear on submit
 });
-
-// addBook(bookOne);
-
-// myLibary.forEach(a => {
-//   libTable.textContent = myLibary[a];
-// });
-
-// }
-
-// libTable.appendChild = myLibary;
-
-// console.log(bookOne.info());
