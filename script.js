56deca3e8b18ec1509f2b909fbd67e228874efb1
Luke Spicknall <lukejspicknall@gmail.com>
2023-03-18 20:34:06 -0400

    style table. change displayBook() so that read value is checkbox in table. style side-form.

--- script.js
+++ script.js
@@ -15,30 +15,40 @@ class Book {
 const myLibary = Object.values(Book); // creates array of Book objects/values
 
 function displayBook() {
+  // puts Book{} data from myLibrary[] into libTable
   tBody.innerHTML = ''; // clears current tbody to avoid repeats
   for (let i = 0; i < myLibary.length; i += 1) { // loop throu []
     const row = document.createElement('tr'); // create new tr for new Book
     tBody.appendChild(row); // add that tr to tbody in libTable
-    const titleCell = document.createElement('td');
-    const authorCell = document.createElement('td');
+    const titleCell = document.createElement('td'); //
+    const authorCell = document.createElement('td'); //
     const pagesCell = document.createElement('td'); // create new cells for Book data
-    const readCell = document.createElement('td');
-    titleCell.textContent = myLibary[i].title;
-    authorCell.textContent = myLibary[i].author;
+    const readCell = document.createElement('td'); //
+    titleCell.textContent = myLibary[i].title; //
+    authorCell.textContent = myLibary[i].author; //
     pagesCell.textContent = myLibary[i].pages; // assing object value to cell content
-    readCell.textContent = myLibary[i].read;
-    row.appendChild(titleCell);
-    row.appendChild(authorCell);
+    row.appendChild(titleCell); //
+    row.appendChild(authorCell); //
     row.appendChild(pagesCell); // add those cells to the new row
+    if (myLibary[i].read = 'no') {
+      const unChecked = document.createElement('input');
+      unChecked.setAttribute('type', 'checkbox');
+      readCell.appendChild(unChecked);
+    } else if (myLibary[i].read = 'yes') {
+      const checkedUp = document.createElement('input');
+      checkedUp.setAttribute('type', 'checkbox');
+      document.querySelector('checkedbox').checked = 'true';
+      readCell.appendChild(checkedUp);
+    }
     row.appendChild(readCell);
   }
 }
 
 function addBook() {
-  const title = document.querySelector('#book-title').value;
-  const author = document.querySelector('#author-name').value;
+  const title = document.querySelector('#book-title').value; //
+  const author = document.querySelector('#author-name').value; //
   const pages = document.querySelector('#page-count').value; // find value of each form input
-  const read = document.querySelector('input[name="have-read"]').value;
+  const read = document.querySelector('input[name="have-read"]:checked').value; //
   const newBook = new Book(title, author, pages, read); // creates new Book object with those values
   myLibary.push(newBook); // pushes that new book object into myLib[]
   displayBook();
