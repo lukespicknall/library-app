f94347d1826cc6055c8ff1114bf5854c0355f969
Luke Spicknall <lukejspicknall@gmail.com>
2023-03-28 22:08:50 -0400

    **BREAKTHROUGH** In displayBook(), set data-id attribute on each creation instance of readCheck (checkbox) and deleteImg (delete button) so that each element's data-id matches its associated Book object's array index. Create clickRead() and clickDelete() functions that take the data-id of the click event target and pass it to its respective readBook() and deleteBook() functions so that the action taken on the HTML Table element alters it's associated Book{} in myLibrary[]. Objects now truly update from HTML events. Remove all readBook() and deleteBook() instances from displayBook() to eliminate any unnecessary hoisting.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// const libTable = document.querySelector('libTable');
 const tBody = document.querySelector('tbody');
 const addBtn = document.querySelector('.add-btn');
 const sideForm = document.querySelector('.side-form');
 const formOverlay = document.querySelector('.form-overlay');
 
 class Book {
-  constructor(title, author, pages, read = false) {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -15,35 +14,22 @@ class Book {
 
 const myLibary = Object.values(Book); // creates array of Book objects/values
 
-// runsfunction on specific index assigned to object when created
-function readBook(i) {
-  const checker = document.querySelector('.read-check'); // looks at readCheck checkbox created in displayBook()
-  if (checker.checked === false) {
-    // if unchecked after click
-    myLibary[i].read = 'no'; // set objects read value to match
-  } else if (checker.checked === true) {
-    // if checked acter click
-    myLibary[i].read = 'yes'; // set objects read value to match
-  }
-}
-
 // puts Book{} data from myLibrary[] into libTable
 function displayBook() {
   tBody.innerHTML = ''; // clears current tbody to avoid repeats
   for (let i = 0; i < myLibary.length; i += 1) {
-    // loop throu []
+    // loop throu myLibrary[]
     const row = document.createElement('tr'); // create new tr for new Book
     row.className = 'table-row';
-    row.setAttribute('data-id', [i]);
     tBody.appendChild(row); // add that tr to tbody in libTable
     const titleCell = document.createElement('td'); //
     const authorCell = document.createElement('td'); //
     const pagesCell = document.createElement('td'); // create new cells for Book data
     const readCell = document.createElement('td'); //
     const deleteCell = document.createElement('td'); //
-    pagesCell.className = 'pages-cell';
-    readCell.className = 'read-cell';
-    deleteCell.className = 'delete-cell';
+    pagesCell.className = 'pages-cell'; //
+    readCell.className = 'read-cell'; // give class names to table cells
+    deleteCell.className = 'delete-cell'; //
     titleCell.textContent = myLibary[i].title; //
     authorCell.textContent = myLibary[i].author; // assign object value to cell content
     pagesCell.textContent = myLibary[i].pages; //
@@ -54,51 +40,32 @@ function displayBook() {
     // Read Checkbox
     const readCheck = document.createElement('input'); // create an input element
     readCheck.setAttribute('type', 'checkbox'); // make it a checkbox
+    readCheck.setAttribute('data-id', [i]); // assign data-id that = object's array index
     readCheck.className = 'read-check'; // give it a class name
     if (myLibary[i].read === 'no') {
       // if not read
-      // readCheck.checked = 'false';
       readCell.appendChild(readCheck); // add the default unchecked box to the cell
     } else if (myLibary[i].read === 'yes') {
       // if read
       readCheck.checked = 'true'; // make the checkbox checked
-      readCell.appendChild(readCheck); // add the checked box to thec cell
+      readCell.appendChild(readCheck); // add the checked box to the readCell
     }
-
-    //
-    readCheck.addEventListener('click', () => {
-      // ** IMPORTANT sets read click event
-      readBook([i]); // ** IMPORTANT sets this index instance as the parameter.
-    }); // ** IMPORTANT this is how to change read status on a specific object from myLibrary[]
-    //
-
-    row.appendChild(readCell); // add read cell to row
+    row.appendChild(readCell); // add readCell to row
 
     // Delete Button
     const deleteBtn = document.createElement('button'); // create a button
     const deleteImg = document.createElement('img'); // create and img element
     deleteBtn.className = 'delete-btn'; // give button a class
     deleteImg.src = 'images/trash-can-outline.png'; // set img source
-    //
-    deleteImg.addEventListener('click', () => {
-      // ** IMPORTANT sets delete click event
-      deleteRow([i]); // ** IMPORTANT sets this index instance as the parameter.
-    }); // ** IMPORTANT this is how can delete a specific object from myLibrary[]
-    //
     deleteImg.className = 'delete-img'; // give img element a class
+    deleteImg.setAttribute('data-id', [i]); // assign data-id that = object's array index
     deleteBtn.appendChild(deleteImg); // add image to button
     deleteCell.appendChild(deleteBtn); // add button to cell
     row.appendChild(deleteCell); // add cell to row
   }
 }
 
-function deleteRow(index) {
-  if (myLibary.length >= 1) {
-    myLibary.splice(index, 1); // deletes the index imput from myLibrary[]
-    displayBook();
-  }
-}
-
+// creates new {Book}, adds it to myLibrary[], displays updated myLibrary[]
 function addBook() {
   const title = document.querySelector('#book-title').value; //
   const author = document.querySelector('#author-name').value; //
@@ -107,14 +74,65 @@ function addBook() {
   const newBook = new Book(title, author, pages, read); // creates new Book object with those values
   myLibary.push(newBook); // pushes that new book object into myLib[]
   displayBook();
-  // deleteRow();
 }
 
+// runs function specific object related to element ID passed in clickRead()
+function readBook(readBox, boxId) {
+  if (readBox.checked === false) {
+    // if unchecked after click
+    myLibary[boxId].read = 'no'; // set objects read value to match
+  } else if (readBox.checked === true) {
+    // if checked acter click
+    myLibary[boxId].read = 'yes'; // set objects read value to match
+  }
+}
+
+// removes {Book} from myLibrary[]
+function deleteBook(deleteBoxID) {
+  // takes data-id linked to element clicked
+  if (myLibary.length >= 1) {
+    // if there is t least 1 book
+    myLibary.splice(deleteBoxID, 1); // use data-id as [] index to remove object
+    displayBook(); // display updated array with {Book} removed
+  }
+}
+
+// listens to Read? click and passes specific target values to readBook()
+function clickRead() {
+  document.addEventListener('click', (e) => {
+    // listen for click
+    const readBox = e.target; // set var to element clicked
+    const readBoxId = e.target.dataset.id; // set var to ID associated w/ el clicked
+    if (e.target.className === 'read-check') {
+      // if that el was the read checkbox
+      readBook(readBox, readBoxId); // pass the el and it's ID to readBook()
+    }
+  });
+}
+
+// listens to delet icon click and passes specific target values to delete Book()
+function clickDelete() {
+  document.addEventListener('click', (e) => {
+    // listen for click
+    const deleteBoxID = e.target.dataset.id; // set var to ID associated w/ el clicked
+    if (e.target.className === 'delete-img') {
+      // if that el was the delete img (trash can)
+      deleteBook(deleteBoxID); // pass the el and it's ID to deleteBook()
+    }
+  });
+}
+
+clickRead();
+
+clickDelete();
+
+// calls form when +Add Book is clicked
 addBtn.addEventListener('click', () => {
   sideForm.reset(); // clears form inputs from previous submission
   formOverlay.style.display = 'flex'; // makes form appear
 });
 
+// passes form data to addBook() and hides form when Submit is clicked
 sideForm.addEventListener('submit', (e) => {
   e.preventDefault(); // stops sumbit from sending data to server by default
   addBook(); // sends data to myLib[]
